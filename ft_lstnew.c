7c1af9141eba76d35dd9335a72c636d408e0ce05
jukerste <jukerste@student.codam.nl>
2025-02-25 19:26:18 +0100

    lsnew added

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jukerste <jukerste@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kat <kat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 17:13:15 by jukerste          #+#    #+#             */
-/*   Updated: 2025/02/25 17:32:55 by jukerste         ###   ########.fr       */
+/*   Updated: 2025/02/25 19:24:55 by kat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	
+	t_list *new;
+
+	new = malloc(sizeof(t_list));
+	if (new == NULL)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
 }
