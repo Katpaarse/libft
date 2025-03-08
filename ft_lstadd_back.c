80a0bc2652a0a01ebda8b564eddfa38322ead015
Julian Kersten <jukerste@f1r6s6.codam.nl>
2025-03-08 17:01:32 +0100

    fixed lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: jukerste <jukerste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 16:28:45 by jukerste          #+#    #+#             */
-/*   Updated: 2025/03/08 16:01:11 by jukerste         ###   ########.fr       */
+/*   Updated: 2025/03/08 17:01:10 by jukerste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 
 	if (lst == NULL || new == NULL)
 		return ;
-	if (lst != NULL)
+	if (*lst == NULL)
 	{
 		*lst = new;
 		return ;
