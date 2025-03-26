87d560de01281cfbab298b27c4bc0124a6990f7f
Julian Kersten <jukerste@f0r6s12.codam.nl>
2025-03-26 16:10:41 +0100

    made norm proof

--- ft_atol.c
+++ ft_atol.c
@@ -6,13 +6,13 @@
 /*   By: jukerste <jukerste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 15:51:14 by jukerste          #+#    #+#             */
-/*   Updated: 2025/03/26 15:51:28 by jukerste         ###   ########.fr       */
+/*   Updated: 2025/03/26 16:09:57 by jukerste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-long ft_atol_converter (char const *str, int i, int sign)
+long	ft_atol_converter(char const *str, int i, int sign)
 {
 	long	num;
 
@@ -37,15 +37,15 @@ long ft_atol_converter (char const *str, int i, int sign)
 		num = num * 10 + (str[i] - '0');
 		i++;
 	}
-	return (num *sign);
+	return (num * sign);
 }
 
-long ft_atol(char const *str)
+long	ft_atol(char const *str)
 {
 	int		i;
 	int		sign;
 
 	i = 0;
 	sign = 1;
-	return ft_atol_converter(str, i , sign);
+	return (ft_atol_converter(str, i, sign));
 }
