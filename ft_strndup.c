2c210b76c2a100880bd23f2f8cd7cd401a800e85
Julian Kersten <jukerste@f1r4s17.codam.nl>
2025-08-12 16:30:35 +0200

    added strndup

---
+++ ft_strndup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jukerste <jukerste@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 16:25:28 by jukerste          #+#    #+#             */
+/*   Updated: 2025/08/12 16:28:18 by jukerste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strndup(char const *s, size_t n)
+{
+	size_t	len;
+	size_t	i;
+	char	*dup;
+
+	len = 0;
+	while (s[len] && len < n)
+		len++;
+	dup = malloc(len + 1);
+	if (dup == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[len] = '\0';
+	return (dup);
+}
